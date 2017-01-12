0777c218b0d604b1eec596bd846f8f122aa2d91a
Hiroshi Hatake <hatake@clear-code.com>
2017-01-12 15:11:12 +0900

    Indent

--- lib/fluent/plugin/in_systemd.rb
+++ lib/fluent/plugin/in_systemd.rb
@@ -4,86 +4,86 @@ require "fluent/plugin/systemd/pos_writer"
 
 module Fluent
   module Plugin
-  class SystemdInput < Input
-    Fluent::Plugin.register_input("systemd", self)
+    class SystemdInput < Input
+      Fluent::Plugin.register_input("systemd", self)
 
-    config_param :path, :string, default: "/var/log/journal"
-    config_param :filters, :array, default: []
-    config_param :pos_file, :string, default: nil
-    config_param :read_from_head, :bool, default: false
-    config_param :strip_underscores, :bool, default: false
-    config_param :tag, :string
+      config_param :path, :string, default: "/var/log/journal"
+      config_param :filters, :array, default: []
+      config_param :pos_file, :string, default: nil
+      config_param :read_from_head, :bool, default: false
+      config_param :strip_underscores, :bool, default: false
+      config_param :tag, :string
 
-    def configure(conf)
-      super
-      @pos_writer = PosWriter.new(@pos_file)
-      @journal = Systemd::Journal.new(path: @path)
-      @journal.filter(*@filters)
-      seek
-    end
+      def configure(conf)
+        super
+        @pos_writer = PosWriter.new(@pos_file)
+        @journal = Systemd::Journal.new(path: @path)
+        @journal.filter(*@filters)
+        seek
+      end
 
-    def start
-      super
-      @running = true
-      @pos_writer.start
-      @thread = Thread.new(&method(:run))
-    end
+      def start
+        super
+        @running = true
+        @pos_writer.start
+        @thread = Thread.new(&method(:run))
+      end
 
-    def shutdown
-      super
-      @running = false
-      @thread.join
-      @pos_writer.shutdown
-    end
+      def shutdown
+        super
+        @running = false
+        @thread.join
+        @pos_writer.shutdown
+      end
 
-    private
+      private
 
-    def seek
-      seek_to(@pos_writer.cursor || read_from)
-    rescue Systemd::JournalError
-      log.warn("Could not seek to cursor #{@pos_writer.cursor} found in pos file: #{@pos_writer.path}")
-      seek_to(read_from)
-    end
+      def seek
+        seek_to(@pos_writer.cursor || read_from)
+      rescue Systemd::JournalError
+        log.warn("Could not seek to cursor #{@pos_writer.cursor} found in pos file: #{@pos_writer.path}")
+        seek_to(read_from)
+      end
 
-    # according to https://github.com/ledbettj/systemd-journal/issues/64#issuecomment-271056644
-    # and https://bugs.freedesktop.org/show_bug.cgi?id=64614, after doing a seek(:tail),
-    # you must move back in such a way that the next move_next will return the last
-    # record
-    def seek_to(pos)
-      @journal.seek(pos)
-      return unless pos == :tail
-      @journal.move(-2)
-    end
+      # according to https://github.com/ledbettj/systemd-journal/issues/64#issuecomment-271056644
+      # and https://bugs.freedesktop.org/show_bug.cgi?id=64614, after doing a seek(:tail),
+      # you must move back in such a way that the next move_next will return the last
+      # record
+      def seek_to(pos)
+        @journal.seek(pos)
+        return unless pos == :tail
+        @journal.move(-2)
+      end
 
-    def read_from
-      @read_from_head ? :head : :tail
-    end
+      def read_from
+        @read_from_head ? :head : :tail
+      end
 
-    def run
-      Thread.current.abort_on_exception = true
-      watch do |entry|
-        begin
-          router.emit(@tag, Fluent::EventTime.from_time(entry.realtime_timestamp), formatted(entry))
-        rescue => e
-          log.error("Exception emitting record: #{e}")
+      def run
+        Thread.current.abort_on_exception = true
+        watch do |entry|
+          begin
+            router.emit(@tag, Fluent::EventTime.from_time(entry.realtime_timestamp), formatted(entry))
+          rescue => e
+            log.error("Exception emitting record: #{e}")
+          end
+        end
+      end
+
+      def formatted(entry)
+        return entry.to_h unless @strip_underscores
+        Hash[entry.to_h.map { |k, v| [k.gsub(/\A_+/, ""), v] }]
+      end
+
+      def watch
+        while @running
+          next unless @journal.wait(1_000_000)
+          while @journal.move_next && @running
+            yield @journal.current_entry
+            @pos_writer.update(@journal.cursor)
+          end
         end
       end
     end
-
-    def formatted(entry)
-      return entry.to_h unless @strip_underscores
-      Hash[entry.to_h.map { |k, v| [k.gsub(/\A_+/, ""), v] }]
-    end
-
-    def watch
-      while @running
-        next unless @journal.wait(1_000_000)
-        while @journal.move_next && @running
-          yield @journal.current_entry
-          @pos_writer.update(@journal.cursor)
-        end
-      end
-    end
-  end
   end
 end
--- lib/fluent/plugin/systemd/pos_writer.rb
+++ lib/fluent/plugin/systemd/pos_writer.rb
@@ -2,61 +2,61 @@ require "fluent/plugin/input"
 
 module Fluent
   module Plugin
-  class SystemdInput < Input
-    class PosWriter
-      def initialize(pos_file)
-        @path = pos_file
-        @lock = Mutex.new
-        @cursor = nil
-        @written_cursor = nil
-        setup
-      end
-
-      attr_reader :cursor, :path
-
-      def start
-        return unless @path
-        @running = true
-        @thread = Thread.new(&method(:work))
-      end
-
-      def shutdown
-        return unless @path
-        @running = false
-        @thread.join
-        write_pos
-      end
-
-      def update(c)
-        return unless @path
-        @lock.synchronize { @cursor = c }
-      end
-
-      private
-
-      def setup
-        return unless @path && File.exist?(@path)
-        @cursor = IO.read(@path).chomp
-      end
-
-      def work
-        while @running
-          write_pos
-          sleep 1
+    class SystemdInput < Input
+      class PosWriter
+        def initialize(pos_file)
+          @path = pos_file
+          @lock = Mutex.new
+          @cursor = nil
+          @written_cursor = nil
+          setup
         end
-      end
 
-      def write_pos
-        @lock.synchronize do
-          if @written_cursor != @cursor
-            file = File.open(@path, "w+")
-            file.print @cursor
-            file.close
-            @written_cursor = @cursor
+        attr_reader :cursor, :path
+
+        def start
+          return unless @path
+          @running = true
+          @thread = Thread.new(&method(:work))
+        end
+
+        def shutdown
+          return unless @path
+          @running = false
+          @thread.join
+          write_pos
+        end
+
+        def update(c)
+          return unless @path
+          @lock.synchronize { @cursor = c }
+        end
+
+        private
+
+        def setup
+          return unless @path && File.exist?(@path)
+          @cursor = IO.read(@path).chomp
+        end
+
+        def work
+          while @running
+            write_pos
+            sleep 1
+          end
+        end
+
+        def write_pos
+          @lock.synchronize do
+            if @written_cursor != @cursor
+              file = File.open(@path, "w+")
+              file.print @cursor
+              file.close
+              @written_cursor = @cursor
+            end
           end
         end
       end
     end
-  end
   end
 end
